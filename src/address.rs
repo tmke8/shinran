e73450e2bbfa5e52883fc3f7d4e48e281268b176
Thomas M Kehrenberg <tmke8@posteo.net>
2024-04-05 19:00:05 +0200

    Add some code

---
+++ src/address.rs
@@ -0,0 +1,104 @@
+use std::env;
+use std::fs;
+use std::io;
+use std::path::PathBuf;
+
+pub fn get_address() -> io::Result<String> {
+    if let Ok(address) = env::var("IBUS_ADDRESS") {
+        if !address.is_empty() {
+            return Ok(address);
+        }
+    }
+
+    let path = get_socket_path()?;
+    let data = fs::read_to_string(path)?;
+
+    let mut address = "";
+    for line in data.lines() {
+        if line.starts_with("IBUS_ADDRESS=") {
+            address = &line[13..];
+        }
+    }
+
+    Ok(address.to_string())
+}
+
+fn get_socket_path() -> io::Result<PathBuf> {
+    if let Ok(path) = env::var("IBUS_ADDRESS_FILE") {
+        if !path.is_empty() {
+            return Ok(PathBuf::from(path));
+        }
+    }
+
+    let wayland_display = env::var("WAYLAND_DISPLAY");
+    let display: Option<String>;
+    let (display_number, hostname) = match &wayland_display {
+        Ok(val) if !val.is_empty() => (val.as_str(), None),
+        _ => {
+            display = env::var("DISPLAY").ok();
+            match &display {
+                Some(display) if !display.is_empty() => {
+                    let (hostname, display_number) =
+                        parse_display_string(&display).ok_or_else(|| {
+                            eprintln!("Failed to parse DISPLAY: {}", display);
+                            io::Error::new(io::ErrorKind::InvalidData, "Failed to parse DISPLAY")
+                        })?;
+                    let hostname = if hostname.is_empty() {
+                        None
+                    } else {
+                        Some(hostname)
+                    };
+                    (display_number, hostname)
+                }
+                _ => {
+                    eprintln!("DISPLAY is empty! We use default DISPLAY (:0.0)");
+                    ("0", None)
+                }
+            }
+        }
+    };
+
+    let hostname = hostname.unwrap_or("unix");
+
+    let machine_id = get_machine_id()?;
+    let path = get_user_config_dir().join(format!(
+        "ibus/bus/{}-{}-{}",
+        machine_id, hostname, display_number
+    ));
+
+    Ok(path)
+}
+
+/// Parse a string of the format "{hostname}:{displaynumber}.{screennumber}".
+fn parse_display_string(s: &str) -> Option<(&str, &str)> {
+    // Split the string at the first ':'
+    let (hostname, display_screen) = s.split_once(':')?;
+    // Split the remaining part at the first '.'
+    let (display_number, _screen_number) = display_screen.split_once('.')?;
+    // If everything went well, return the hostname and display number
+    Some((hostname, display_number))
+}
+
+fn get_machine_id() -> io::Result<String> {
+    let mut id = match fs::read_to_string("/var/lib/dbus/machine-id") {
+        Ok(id) => id,
+        Err(_) => {
+            let id = fs::read_to_string("/etc/machine-id")?;
+            id
+        }
+    };
+
+    // Trim leading and trailing whitespaces in-place.
+    let end = id.trim_end().len();
+    id.truncate(end);
+    let trimmed_len = id.trim_start().len();
+    id.replace_range(..(id.len() - trimmed_len), "");
+    Ok(id)
+}
+
+fn get_user_config_dir() -> PathBuf {
+    match env::var("XDG_CONFIG_HOME") {
+        Ok(val) => PathBuf::from(val),
+        Err(_) => PathBuf::from(env::var("HOME").unwrap()).join(".config"),
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,78 @@
-fn main() {
-    println!("Hello, world!");
+use event_listener::{Event, Listener};
+use zbus::connection;
+use zbus::object_server::SignalContext;
+use zbus::{interface, AuthMechanism};
+
+mod address;
+// mod input_context;
+
+const REQUEST_NAME: &str = "org.freedesktop.IBus.Shinran";
+const SERVE_AT: &str = "/org/freedesktop/IBus/Engine/Shinran";
+
+struct ShinranEngine {
+    done: Event,
+    text: String,
+    cursor_pos: u32,
+}
+
+#[interface(name = "org.freedesktop.IBus.Engine")]
+impl ShinranEngine {
+    /// ProcessKeyEvent method
+    async fn process_key_event(
+        &self,
+        #[zbus(signal_context)] ctxt: SignalContext<'_>,
+        keyval: u32,
+        keycode: u32,
+        state: u32,
+    ) -> bool {
+        println!("process_key_event: keyval={}, keycode={}, state={}", keyval, keycode, state);
+        true
+    }
+    ///
+    /// FocusIn method
+    fn focus_in(&self) {}
+
+    /// FocusOut method
+    fn focus_out(&self) {}
+
+    /// Destroy method
+    async fn destroy(&self) {}
+
+    /// UpdatePreeditText signal
+    #[zbus(signal)]
+    async fn update_preedit_text(
+        ctxt: &SignalContext<'_>,
+        text: zbus::zvariant::Value<'_>,
+        cursor_pos: u32,
+        visible: bool,
+    ) -> zbus::Result<()>;
+
+    /// CommitText signal
+    #[zbus(signal)]
+    async fn commit_text(
+        ctxt: &SignalContext<'_>,
+        text: zbus::zvariant::Value<'_>,
+    ) -> zbus::Result<()>;
+}
+
+#[async_std::main]
+async fn main() -> zbus::Result<()> {
+    let engine = ShinranEngine {
+        done: Event::new(),
+        text: "".to_string(),
+        cursor_pos: 0,
+    };
+    let done_listener = engine.done.listen();
+
+    let address = address::get_address()?;
+    let conn = connection::Builder::address(address.as_str())?
+        .auth_mechanisms(&[AuthMechanism::External, AuthMechanism::Cookie])
+        .name(REQUEST_NAME)?
+        .serve_at(SERVE_AT, engine)?
+        .build()
+        .await?;
+
+    done_listener.wait();
+
+    Ok(())
 }
